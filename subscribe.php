e6713fe0736e1a850e322314fb3bf948f084bbec
Vincent Jousse <vincent@jousse.org>
2013-03-09 14:41:28 +0100

    Add subscribe template

---
+++ subscribe.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Subscribe
+*/
+get_header();
+?>
+    <div class="twelve columns">
+        <h1><?php the_title(); ?></h1>
+
+        <?php the_content(); ?>
+
+    </div>
+
+
+<?php
+get_footer();
+?>
